b14ba07122a999b2555f0659aad788fb77dd7256
ipsgiraf <shainu.giraf@gmail.com>
2024-12-06 17:39:20 +0530

    meeting report excel

--- resources/views/meetings/mtmg.blade.php
+++ resources/views/meetings/mtmg.blade.php
@@ -1,3 +1,4 @@
+@php($mtmg = 0)
 <table border="1" style="border-collapse: collapse; width: 100%;">
     <thead>
         <tr>
@@ -29,15 +30,29 @@
 
             {{-- Meeting Grades --}}
             @foreach($meetings['grades'] as $grade)
-            <td>{{ $grade }}</td>
+            <td style="text-align: right;">{{ $grade }}</td>
             @endforeach
             @for($i = count($meetings['grades']); $i < $maxMeetings; $i++)
             <td></td>
             @endfor
 
-            {{-- Total Grade (DTMG) --}}
-            <td>{{ $meetings['total'] }}</td>
+            {{-- Daily Total Grade (DTMG) --}}
+            <td style="text-align: right;">{{ $meetings['total'] }}</td>
         </tr>
+        @php($mtmg += $meetings['total'])
         @endforeach
+        <tr>
+            @for($i = 1; $i <= (2 * $maxMeetings); $i++)
+            <td></td>
+
+            @if($i == 6)
+            <td style="background-color: #92d050; font-weight: bold; border: 1px solid black;">MTMG</td>
+            @endif
+
+            @if($i == (2 * $maxMeetings))
+            <td style="text-align: right; border: 1px solid black;">{{$mtmg}}</td>
+            @endif
+            @endfor
+        </tr>
     </tbody>
 </table>
